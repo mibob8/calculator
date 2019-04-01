8502f95219422a626bf1eb551f6aa1b6858d0ca9
Michal Bobrowski <michal.bobrowski@op.pl>
2019-04-01 20:08:48 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the location of the text file with parameters and press any key");
+            var parametersFile = Console.ReadLine();
+
+            var calculationResult = Calculate(parametersFile);
+
+            Console.WriteLine($@"Your result is {calculationResult}");
+            Console.ReadKey();
+
+        }
+
+        private static double Calculate(string parametersFile)
+        {
+            double result;
+
+
+
+
+
+
+
+
+            return result;
+        }
+
+
+
+
+
+    }
+}
